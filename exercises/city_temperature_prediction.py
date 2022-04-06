1b93ca8f484a224b15a2e9e601f507ade7a6b7ac
דניאל אזולאי <daniel.azulay@mail.huji.ac.il>
2022-04-06 21:17:23 +0300

    finished israeli temperatures graph in ex2

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
+import os.path
 pio.templates.default = "simple_white"
 
 
@@ -21,17 +22,27 @@ def load_data(filename: str) -> pd.DataFrame:
     -------
     Design matrix and response vector (Temp)
     """
-    raise NotImplementedError()
+    data = pd.read_csv(filename, parse_dates=['Date'])
+    data = data.dropna()
+    data = data.drop(data.index[(data.Day < 0) | (data.Month < 0) | (data.Year < 0) | (data.Day > 31) |
+                                (data.Month > 12) | (data.Temp < -20)])
+    temp_col = data.pop('Temp')
+    data['DayOfYear'] = data.Date.apply(lambda x: x.dayofyear)
+    data['Temp'] = temp_col
+
+    return data
 
 
 if __name__ == '__main__':
     np.random.seed(0)
     # Question 1 - Load and preprocessing of city temperature dataset
-    raise NotImplementedError()
+    path = '..\datasets\City_Temperature.csv'
+    samples = load_data(os.path.join(os.path.dirname(__file__), path))
 
     # Question 2 - Exploring data for specific country
-    raise NotImplementedError()
-
+    israeli_samples = samples.loc[samples['Country'] == 'Israel']
+    fig = px.scatter(israeli_samples, x='DayOfYear', y='Temp', color=israeli_samples['Year'].astype(str),
+                     height=600, width=800).show()
     # Question 3 - Exploring differences between countries
     raise NotImplementedError()
 
